10178212fd02e4d137bfb400754e24357d36ce05
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-04-26 20:02:16 +0200

    create zoldseges map and put some item

---
+++ src/hazi/Main.java
@@ -0,0 +1,17 @@
+package hazi;
+
+import java.util.HashMap;
+
+public class Main {
+static HashMap<String, Integer> zoldseges = new HashMap<String,Integer>();
+	public static void main(String[] args) {
+		
+		zoldseges.put("Eper", 2900);
+		zoldseges.put("Alma", 590);
+		zoldseges.put("Burgonya", 350);
+		zoldseges.put("Cukkini", 890);
+		zoldseges.put("Gomba", 750);
+		zoldseges.put("Kivi", 850);
+
+	}
+}
